11edf0ce4ae36c3db5e4b247d1069f723bc960ca
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-06 17:41:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main(){
+  int x ;
+    cin >> x ;
+    x=conVL(x) ;
+    bool y=convL(x );
+    if (y==true) {
+        cout << (char)x << endl;
+    }
+    else {
+        cout << "errore" << endl;
+    }
   return 0;
 }
