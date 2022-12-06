b3435981a1f9cfb30333aea380a223c84119efb7
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-06 23:31:35 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,18 @@
 #include "lib.h"
 
 bool convL(int &a) {
-   if (a>=65 and a<=90) {
-       a+=32 ;
-       return true ;
-   }
-   else if (a>=97 and a<=122) {
-       a-=32 ;
-       return true ;
-   }
-   else {
+    bool i=0 ;
+    if ((a>=65 and a<=90) or (a>=97 and a<=122)) {
+        i==1 ;
+        if ( (i=1) and (a>=65 and a<=90)) {
+            a += 32;
+        }
+        else if ((i=1) and (a>=97 and a<=122)) {
+            a-=32 ;
+        }
+        return true ;
+    }
+    else {
        return false ;
    }
 }
