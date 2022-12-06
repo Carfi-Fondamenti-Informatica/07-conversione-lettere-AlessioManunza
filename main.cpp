108d4e89ac4164d761b111e1ac96c475f38efdd5
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-07 00:22:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include "lib.h"
-
+using namespace std ;
 int main() {
     int x ;
-    std::cin >> x ;
+    cin >> x ;
     char a=x ;
     bool y= convL(a) ;
     if (y) {
-        std::cout << a << std::endl;
+        cout << a << std::endl;
     }
     else{
         std::cout << "errore" << std::endl;
     }
-
 }
