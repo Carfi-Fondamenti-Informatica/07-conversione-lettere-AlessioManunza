2a72622f2e23df9a6e562bf4589aa76f25aebe84
AlessioManunza <115411214+AlessioManunza@users.noreply.github.com>
2022-12-07 00:23:09 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,10 +2,10 @@
 
 bool convL(char &a) {
     (int) a ;
-    if ((a>=65 and a<=90) or (a>=97 and a<=122)) {
+    if (a>=0 and a<=255) {
         if (a>=65 and a<=90) {
-            a=(char)(a+32) ;
-            }
+            a = (char) (a + 32);
+        }
         else if (a>=97 and a<=122) {
             a=(char)(a-32);
         }
